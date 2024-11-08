c35191c065f2ce9595d9f79083877769f99e44f4
Michael Jiang <mjiang@lcs.on.ca>
2024-11-08 09:02:04 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Michael on 2024/11/8.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
